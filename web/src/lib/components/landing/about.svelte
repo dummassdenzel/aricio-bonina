e984af22ba2dd2e8795c8920142752799a1f393c
0.8lili <130875766+reesenuts@users.noreply.github.com>
2024-11-20 01:13:35 +0800

    about (done)

--- web/src/lib/components/landing/about.svelte
+++ web/src/lib/components/landing/about.svelte
@@ -1,3 +1,30 @@
-<section>
-    <h1>About Section</h1>
+<section class="flex flex-col md:flex-row items-center gap-20 p-6 md:p-24 bg-back rounded-2xl">
+    <!-- about us -->
+    <div class="w-full md:w-1/2 text-center md:text-justify">
+      <h2 class="text-2xl md:text-2xl font-bold text-teal mb-4">About Us</h2>
+
+      <!-- descriptions -->
+      <p class="text-slate leading-relaxed mb-4">Welcome to Aricio Real Estate. We provide comfortable and affordable apartment units in Olongapo City, proudly serving the community for over <span class="font-medium">10</span> years since our founding in <span class="font-medium">2014</span>.</p>
+      <p class="text-slate leading-relaxed mb-4">Owned by <span class="font-medium">Aricio D. Bonina</span>, we offer cozy living spaces designed for individuals and families. Our apartments are conveniently located near key destinations like <span class="font-medium">Magsaysay Business District</span>, <span class="font-medium">SM City Olongapo Downtown</span>, <span class="font-medium">SM City Central</span>, and the <span class="font-medium">Subic Bay Freeport Zone</span>, offering the perfect mix of accessibility and comfort.</p>
+      <p class="text-slate leading-relaxed mb-4">At Aricio Real Estate, we make finding your ideal home simple and stress-free.</p>
+    
+      <!-- location -->
+      <p class="text-teal leading-relaxed font-semibold">Sea Pearl Residences</p>
+      <p class="text-slate leading-relaxed text-sm mb-5">69 Gallagher Street, East Tapinac, Olongapo City</p>
+      
+      <!-- ceo profile -->
+      <div class="border-t">
+        <div class="flex items-center gap-4 mt-5">
+            <div class="w-12 h-12 rounded-full bg-drop"></div> 
+            <div>
+              <p class="text-muted text-sm">Founder and CEO, Aricio Bonina Real Estate Leasing</p>
+            </div>
+          </div>
+      </div>
+    </div>
+
+    <!-- image -->
+    <div class="w-full md:w-1/2 bg-drop rounded-2xl h-80 flex items-center justify-center">
+        <!-- add image here -->
+    </div>
 </section>
